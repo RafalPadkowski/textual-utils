91e99045ef9d07f762360f0734c467af61597fcd
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-01-30 14:40:31 +0100

    feat: refactor AboutHeaderIcon

--- textual_utils/__init__.py
+++ textual_utils/__init__.py
@@ -1,4 +1,4 @@
-from textual_utils.about_header_icon import mount_about_header_icon
+from textual_utils.about_header_icon import AboutHeaderIcon, mount_about_header_icon
 from textual_utils.app_metadata import AppMetadata
 from textual_utils.i18n import _, init_translation, set_translation
 from textual_utils.screens import AboutScreen, ConfirmScreen, SettingsScreen
@@ -9,6 +9,7 @@ __all__ = [
     "set_translation",
     "_",
     "mount_about_header_icon",
+    "AboutHeaderIcon",
     "AppMetadata",
     "AboutScreen",
     "ConfirmScreen",
