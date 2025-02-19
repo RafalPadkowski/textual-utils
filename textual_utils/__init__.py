1f488458dc3028190b58385140f37fb1523c0381
Rafal Padkowski <rafaelp@poczta.onet.pl>
2025-02-19 14:09:55 +0100

    feat: add settings module

--- textual_utils/__init__.py
+++ textual_utils/__init__.py
@@ -3,6 +3,7 @@ from textual_utils.app_metadata import AppMetadata
 from textual_utils.i18n import _, init_translation, set_translation
 from textual_utils.screens import AboutScreen, ConfirmScreen, SettingsScreen
 from textual_utils.setting_row import SettingRow
+from textual_utils.settings import load_settings, save_settings
 
 __all__ = [
     "init_translation",
@@ -13,6 +14,8 @@ __all__ = [
     "AppMetadata",
     "AboutScreen",
     "ConfirmScreen",
+    "load_settings",
+    "save_settings",
     "SettingRow",
     "SettingsScreen",
 ]
